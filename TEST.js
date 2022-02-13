095a843675575be474903e4a5ddb456ea0d0d275
Alex <alexandepp@gmail.com>
2022-02-13 09:13:06 +0300

    full copy

--- TEST.js
+++ TEST.js
@@ -1,133 +1,100 @@
-import axios, {AxiosResponse} from "axios";
-import {NewProfileType} from "../Redux/profileReducer";
+// import axios, {AxiosResponse} from "axios";
+// import {NewProfileType} from "../Redux/profileReducer";
 
 
-const instance = axios.create({
-    withCredentials: true,
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-    headers: {
-        'API-KEY': 'fae6bcdf-1b7b-4b5f-8f9c-eecd7cb26aa8',
-    }
-})
 
 
-export const usersAPI = {
 
-    getUsers(currentPage: number, pageSize: number)  {
 
-        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
-            .then(responce => responce.data)
-    },
-    follow(userId:number){
-        return instance.post(`follow/${userId}`, {}
-        )
-    },
-    unFollow(userId:number){
-        return instance.delete(`follow/${userId}`)
-    }
-}
 
-export const AuthMeAPI = {
-    getAuth() {
-        return instance.get(`auth/me`);
-    },
 
-    loginIn(email:string,password:string, rememberMe:boolean,captcha:string){
-        return instance.post(`auth/login`, {email,password,rememberMe,captcha});
-    },
-    loginOut(){
 
-        return instance.delete(`auth/login`,)
-    },
-    getCaptcha(){
-        return instance.get(`/security/get-captcha-url`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+var user = {
+    name: 'Alex',
+    age: 12,
+    address: {
+        city: {
+            title: 'Minsk'
+        }
     }
 }
-export const profileAPI = {
-    getProfile(userId:number) {
-        return instance.get<NewProfileType>(`profile/${userId}`).then(res=>res.data)
-    },
-    getStatus(userId:string){
-        return instance.get(`profile/status/${userId}`).then(res=>res.data)
-    },
-    updateStatus(status:string){
-        return instance.put(`profile/status/`, {status:status}).then(res=>res.data)
-    },
-    savePhoto(photo:any){
-        const formData=new FormData();
-        formData.append('image', photo)
-        return instance.put<AllResponseType<SavePhotoResponseDataType>>(`profile/photo`, formData, {headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        }).then(res=>res.data);
-    },
-    saveProfile(profile:any){
+console.log(user.address.city.title)
+console.log(user['address']['city']['title'])
 
-        return instance.put(`profile/`, profile).then(res=>res.data)
-    }
+let city = {}
+city.title = 'Grodno'
+console.log(city)
+console.log(city.title)
+
+let users = ['Alex', 'Kate', 'Mike']
+console.log(users[0])
+
+console.log(user['name'] + '--- 0 user')
+
+let usersObj = {
+    '0' : 'A',
+    '1' : 'B',
+    '2' : 'C'
 }
 
-type SavePhotoResponseDataType={
-    photos: PhotosType
-}
+console.log(users[1] + 'usersObj ')
+console.log(Object.values(usersObj) + '  Object.keys ')
+console.log(Object.keys(usersObj) + '  Object.keys ')
 
-type PhotosType={
-    small: string| null
-    large: string| null
-}
-
-export type AllResponseType<D = {}, RC=ResultCodesEnum>={
-    data:D
-    messages: Array<string>
-        resultCode: RC
-}
-
-export enum ResultCodesEnum {
-    Success=0,
-        Error=1
-}
+// const instance = axios.create({
+//     withCredentials: true,
+//     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+//     headers: {
+//         'API-KEY': 'fae6bcdf-1b7b-4b5f-8f9c-eecd7cb26aa8',
+//     }
+// })
 
 
-let pr = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve()
-    }, 3000)
-
-})
-
-console.log(pr)
-
-
+// export const usersAPI = {
 //
-// class DeleteUserAction {
-//     constructor() {
-//         this.type = 'DELETE-USER';
-//         this.payload = {
-//             userId: userId
-//         }
+//     getUsers(currentPage: number, pageSize: number)  {
+//
+//         return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+//             .then(responce => responce.data)
+//     },
+//     follow(userId:number){
+//         return instance.post(`follow/${userId}`, {}
+//         )
+//     },
+//     unFollow(userId:number){
+//         return instance.delete(`follow/${userId}`)
 //     }
 // }
 //
-// const action1 = new DeleteUserAction(121212)
-// console.log(action1)
-
-
-// function hello () {
-//     console.log(`I am ${this.name}`)
-// }
+// export const AuthMeAPI = {
+//     getAuth() {
+//         return instance.get(`auth/me`);
+//     },
 //
-// const userFabric = (name) => {
-//     const user = {
-//         name: name,
-//         date: '1',
-//         hello: hello
+//     loginIn(email:string,password:string, rememberMe:boolean,captcha:string){
+//         return instance.post(`auth/login`, {email,password,rememberMe,captcha});
+//     },
+//     loginOut(){
+//
+//         return instance.delete(`auth/login`,)
+//     },
+//     getCaptcha(){
+//         return instance.get(`/security/get-captcha-url`)
 //     }
-//     return user;
 // }
-//
-// const u1 = userFabric('Alex')
-//
-// u1.hello()
 
 
 //
